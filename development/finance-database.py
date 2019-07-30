29580b8bd8a881c0f07d092308a40b0f6e2cd832
Philip Papasavvas <philip.papasavvas@gmail.com>
2019-07-30 16:12:40 +0100

    Small change to finance-database

--- development/finance-database.py
+++ development/finance-database.py
@@ -11,6 +11,8 @@ import os
 
 import yfinance as yf
 
+if __name__ == "__main__":
+
 # data = yf.download(  # or pdr.get_data_yahoo(...
 #         # tickers list or string as well
 #         tickers = "SPY", \
@@ -48,66 +50,66 @@ import yfinance as yf
 
 # ---------------
 # Financial data
-# ---------------
+    # ---------------
 
-a = yf.download(tickers=['AAPL MSFT'], period="1m", interval="1d")
+    a = yf.download(tickers=['AAPL MSFT'], period="1m", interval="1d")
 
-# Smithson Equity
-c = yf.download(tickers=['SSON.L'], period="ytd", interval="1d")
+    # Smithson Equity
+    c = yf.download(tickers=['SSON.L'], period="ytd", interval="1d")
 
-# Fundsmith Equity I Acc
-d = yf.download(tickers=['0P0000RU81.L'], period="ytd", interval="1d")
+    # Fundsmith Equity I Acc
+    d = yf.download(tickers=['0P0000RU81.L'], period="ytd", interval="1d")
 
-# Gabelli Value Plus & Trust
-e = yf.download(tickers=['GVP.L'], period="ytd", interval="1d")
+    # Gabelli Value Plus & Trust
+    e = yf.download(tickers=['GVP.L'], period="ytd", interval="1d")
 
-ex_dict = {'pp_funds': {'Fundsmith Equity': '0P0000RU81.L',
-                        'Gabelli Value': 'GVP.L'}}
+    ex_dict = {'funds': {'Fundsmith Equity': '0P0000RU81.L',
+                            'Gabelli Value': 'GVP.L'}}
 
-for k,v in ex_dict['pp_funds'].items():
-    print(k,v)
+    for k,v in ex_dict['funds'].items():
+        print(k,v)
 
-data = []
-for k,v in ex_dict['pp_funds'].items():
-    data.append(yf.download(tickers= v, period= "ytd", intervals = "1d"))
-
-
-mapping_list = pd.read_csv(r"C:\Users\Philip\Documents\python\input\security_data\mapping_table.csv")
-
-f_map = mapping_list[['GROUP', 'SECURITY NAME', 'YFINANCE']]
-f_map = f_map[f_map['YFINANCE'].isin(['FALSE',np.nan])]
-
-# --------------
-# Database stuff
-# --------------
-
-# start up the mongoDB
-# connect to Arctic
-from pymongo import MongoClient
-from arctic import Arctic
-
-client = MongoClient('mongodb://localhost:27017/')
-
-store = Arctic(client)
-store.list_libraries() # see what libraries are present
-
-store.initialize_library('security_data')
-
-library = store['security_data']
-library.write('Fundsmith Equity', data[0], metadata={'source':'yahoo finance'})
-library.write('Gabelli Value', data[1], metadata={'source':'yahoo finance'})
-
-library.list_symbols()
-
-item = library.read('Gabelli Value')
-
-#view/access the data
-item.data
+    data = []
+    for k,v in ex_dict['funds'].items():
+        data.append(yf.download(tickers= v, period= "ytd", intervals = "1d"))
 
 
-# ------------
-# JSON reading
-# ------------
+    mapping_list = pd.read_csv(r"C:\Users\Philip\Documents\python\input\security_data\mapping_table.csv")
+
+    f_map = mapping_list[['GROUP', 'SECURITY NAME', 'YFINANCE']]
+    f_map = f_map[f_map['YFINANCE'].isin(['FALSE',np.nan])]
+
+    # --------------
+    # Database stuff
+    # --------------
+
+    # start up the mongoDB
+    # connect to Arctic
+    from pymongo import MongoClient
+    from arctic import Arctic
+
+    client = MongoClient('mongodb://localhost:27017/')
+
+    store = Arctic(client)
+    store.list_libraries() # see what libraries are present
+
+    store.initialize_library('security_data')
+
+    library = store['security_data']
+    library.write('Fundsmith Equity', data[0], metadata={'source':'yahoo finance'})
+    library.write('Gabelli Value', data[1], metadata={'source':'yahoo finance'})
+
+    library.list_symbols()
+
+    item = library.read('Gabelli Value')
+
+    #view/access the data
+    item.data
+
+
+    # ------------
+    # JSON reading
+    # ------------
 
 
 
