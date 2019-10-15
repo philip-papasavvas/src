e324c0d963bda8190e8de19735feab4b90c81656
Philip Papasavvas <philip.papasavvas@gmail.com>
2019-10-15 21:31:08 +0100

    Updated cf_wod_email to accept json configs as import- so a user can set the config outside of the script

--- cf_wod_email.py
+++ cf_wod_email.py
@@ -23,6 +23,27 @@ from bs4 import BeautifulSoup
 
 import pymongo
 
+
+def update_quote_db(mongo_config, quotes_import_json):
+    """Function to update database with quotes, mongo_config and quote_import_json as inputs"""
+
+    user = mongo_config['mongo_user']
+    password = mongo_config['mongo_pwd']
+    mongo_url = mongo_config['url_cluster']
+
+    host_url = "".join(["mongodb+srv://", user, ":", password, "@", mongo_url])
+    client = pymongo.MongoClient(host_url)
+
+    library = client['email']  # type pymongo.database.Database
+    coll = library['quotes']  # collection
+    existing_db = coll.find_one()
+
+    quotes_to_add = dict(quotes_import_json)
+
+    coll.update(existing_db, quotes_import_json)
+
+    print("Successfully added new quotes to the db")
+
 class CF_email():
     """
     Class to send email of daily WOD and inspirational quote to email
@@ -34,8 +55,9 @@ class CF_email():
         json config with email address list
     """
 
-    def __init__(self, json_config):
-        self.config = json_config
+    def __init__(self, email_config, mongo_config):
+
+        self.config = {**email_config, **mongo_config}
 
         date_today_dt = datetime.datetime.now()
         date_today_str = date_today_dt.strftime("%Y%m%d")
@@ -69,7 +91,7 @@ class CF_email():
 
         return workout_bs4_element
 
-    def get_random_quote(self):
+    def get_random_quote(self, user=None, password=None, mongo_url=None):
         """Function to get random quote from database for the email
 
         Returns
@@ -78,7 +100,18 @@ class CF_email():
         """
         # initialise the database
 
-        client = pymongo.MongoClient("mongodb+srv://<user>:<password>@cluster0-iooan.mongodb.net/test?retryWrites=true&w=majority")
+        if user is None:
+            user = self.config['mongo_user']
+
+        if password is None:
+            password = self.config['mongo_pwd']
+
+        if mongo_url is None:
+            mongo_url = self.config['url_cluster']
+
+
+        host_url = "".join(["mongodb+srv://",user,":",password,"@",mongo_url])
+        client = pymongo.MongoClient(host_url)
         # client = pymongo.MongoClient("mongodb+srv://username:<password>@cluster0-iooan.mongodb.net/test?retryWrites=true&w=majority")
 
         library = client['email']  # type pymongo.database.Database
@@ -101,12 +134,12 @@ class CF_email():
 
         msg_body_html = "WOD for {date}".format(date=self.date) + "<p>"
         msg_body_html += str(wod_text) + "<p>"
-        msg_body_html += '<strong>' + quote + '</strong> '
-        msg_body_html += '<em>' + author + '</em>'
+        msg_body_html += "<em>" + quote + "</em>"
+        msg_body_html += "<em>" + author + "</em>"
 
         return msg_body_html
 
-    def send_email(self, body, password='password'):
+    def send_email(self, body, password=None):
         """Function to send email at the final step to the user
         with the information
 
@@ -116,6 +149,8 @@ class CF_email():
                 HTML string
         """
         # password = getpass.getpass()
+        if password is None:
+            password = self.config['source_email_pwd']
 
         message = MIMEMultipart("alternative")
         message["Subject"] = " ".join([self.date, self.config['subject']])
@@ -130,7 +165,7 @@ class CF_email():
         context = ssl.create_default_context()
         with smtplib.SMTP_SSL("smtp.gmail.com", port=465, context=context) as server:
             server.login(message["From"], password)
-            server.sendmail(message["From"], message["To"], message.as_string())
+            server.sendmail(message["From"], message["To"].split(','), message.as_string())
 
     def run(self):
         """Run function to bring it all together"""
@@ -143,14 +178,22 @@ class CF_email():
         self.send_email(body = email_text)
         print("Successfully sent daily WOD email")
 
-
 if __name__ == "__main__":
 
     with open('/Users/philip_p/python/projects/config/cf_email.json') as json_file:
-        config = json.load(json_file)
+        email_config = json.load(json_file)
 
-    rn = CF_email(json_config=config)
+    with open('/Users/philip_p/python/projects/config/mongo.json') as json_file:
+        mongo_config = json.load(json_file)
+
+    rn = CF_email(email_config=email_config, mongo_config=mongo_config)
     rn.run()
+
+    with open('/Users/philip_p/python/projects/import/quotes_db.json') as json_file:
+        quotes_json = json.load(json_file)
+
+    update_quote_db(mongo_config=mongo_config, quotes_import_json=quotes_json)
+
 
 # msg_body = "WOD for {date}: \n {workout}. \n \n {quote} - {person}".format(date=date_today_str,
 #                                                                                  workout=repo_contents[0],
