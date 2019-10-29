d416dfdbcdc69b25e32ae47ebf8f81a10d83c079
Philip Papasavvas <philip.papasavvas@gmail.com>
2019-10-29 08:01:08 +0000

    Added capability to attach an image in CF daily wod email (using MIME object)

--- cf_wod_email.py
+++ cf_wod_email.py
@@ -16,6 +16,7 @@ import smtplib
 import ssl
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+from email.mime.image import MIMEImage
 import getpass
 
 import requests
@@ -84,10 +85,16 @@ class CF_email():
         page = requests.get(website)
         soup = BeautifulSoup(page.text, 'html.parser')  # Create a BeautifulSoup object
 
-        wod_class = "sf1KM0yG_abhAm_QFxP7E" # the tag is defined for location of WOD
+        wod_class = "_6zX5t4v71r1EQ1b1O0nO2 jYZW249J9cFebTPrzuIl0"#"_1txuw01hodoV9pnEgg9vKl _1iFhEb-WHz4xf8bQbBXErH"
+        #"_3ISZlXlYytTnehLYeQWJo1" #"sf1KM0yG_abhAm_QFxP7E" # the tag is defined for location of WOD
         repo = soup.find(class_=wod_class)
-        wod_tag =  "_6zX5t4v71r1EQ1b1O0nO2 LerElceKJ8FwMmx8LCILf" # tag for where WOD is stored
-        workout_bs4_element = repo.find(class_=wod_tag) #beautiful soup element
+        # wod_tag = "_6zX5t4v71r1EQ1b1O0nO2 jYZW249J9cFebTPrzuIl0" \
+                   # "_6zX5t4v71r1EQ1b1O0nO2 LerElceKJ8FwMmx8LCILf" # tag for where WOD is stored
+        workout_bs4_element = repo #repo.find(class_=wod_tag) #beautiful soup element
+
+        if workout_bs4_element is None:
+            workout_bs4_element = "The email has broken for some reason, maybe the tag has changed for the WOD. \n" \
+                                  "Sorry, this will be fixed soon!"
 
         return workout_bs4_element
 
@@ -109,7 +116,6 @@ class CF_email():
 
         if mongo_url is None:
             mongo_url = self.config['url_cluster']
-
 
         host_url = "".join(["mongodb+srv://",user,":",password,"@",mongo_url])
         client = pymongo.MongoClient(host_url)
@@ -138,12 +144,13 @@ class CF_email():
         print("Generating email...")
         msg_body_html = "WOD for {date}".format(date=self.date) + "<p>"
         msg_body_html += str(wod_text) + "<p>"
+        msg_body_html += "<br> <strong> Quote of the day </strong> </br> <p>"
         msg_body_html += "<em>" + quote + " </em>"
         msg_body_html += "<strong>" + author + "</strong>"
 
         return msg_body_html
 
-    def send_email(self, body, password=None):
+    def send_email(self, body, image=False,password=None):
         """Function to send email at the final step to the user
         with the information
 
@@ -156,22 +163,32 @@ class CF_email():
         if password is None:
             password = self.config['source_email_pwd']
 
-        message = MIMEMultipart("alternative")
-        message["Subject"] = " ".join([self.date, self.config['subject']])
-        message["From"] = self.config['source_email']
-        message["To"] = self.config['email_distrib_list']
+        msg = MIMEMultipart("alternative") # MIMEMultipart(_subtype='related')
+        msg["Subject"] = " ".join([self.date, self.config['subject']])
+        msg["From"] = self.config['source_email']
+        msg["To"] = self.config['email_distrib_list']
 
         msg_body_html = body
 
-        html_obj = MIMEText(msg_body_html, "html")
-        message.attach(html_obj)
+        if image:
+            img_data = open('/Users/philip_p/python/projects/crossfit-logo.png', 'rb').read()
+            img = MIMEImage(img_data, 'png')
+            img.add_header('Content-Id', '<cf_logo>')  # angle brackets are important
+            img.add_header("Content-Disposition", "inline", filename="cf_logo")
+
+        body = MIMEText(msg_body_html, "html")
+
+        msg.attach(body)
+
+        if image:
+            msg.attach(img)
 
         print("Sending email...")
 
         context = ssl.create_default_context()
         with smtplib.SMTP_SSL("smtp.gmail.com", port=465, context=context) as server:
-            server.login(message["From"], password)
-            server.sendmail(message["From"], message["To"].split(','), message.as_string())
+            server.login(msg["From"], password)
+            server.sendmail(msg["From"], msg["To"].split(','), msg.as_string())
 
     def run(self):
         """Run function to bring it all together"""
