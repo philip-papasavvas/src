06c5ca3cce987345b8e7f55ba247b3730b146aaf
philip-papasavvas <39246705+philip-papasavvas@users.noreply.github.com>
2020-10-09 17:36:32 +0100

    utils_lists: added docstring with more info at top, added Type Hints (per PEP484). Removed embedded examples in docstring, see unit tests instead

--- src/utils_lists.py
+++ src/utils_lists.py
@@ -1,67 +1,48 @@
-# Created 17 June 2019. Translplanted from utils_generic.py
+"""
+Created 17 Jun 2019
+For examples of the methods, see the unit tests in test_utils_lists
+"""
 from math import ceil
+from typing import Union
 
 
-def list_as_comma_sep(lst):
-    """Gets a list and returns one single string with elements separated by a comma
-    >>> list_as_comma_sep(["hello","this","is","an", "example"]) # 'hello,this,is,an,example'
-    """
+def list_as_comma_sep(lst: list) -> str:
+    """Gets a list and returns one single string with elements separated by a comma"""
     return ",".join(lst)
 
 
-def has_duplicates(lst):
-    """
-    Checks if a list has duplicate values
-    >>> has_duplicates([1,2,4,5]) # False
-    >>> has_duplicates([1,2,2,5]) # True
-    """
+def has_duplicates(lst: list) -> bool:
+    """Checks if a list has duplicate values"""
     return len(lst) != len(set(lst))
 
 
-def all_unique(lst):
-    """
-    Check if a given list has duplicate elements
-    >>> all_unique([1,2,3,4]) # True
-    >>> all_unique([1,2,2,4]) # False
-    """
+def all_unique(lst: list) -> bool:
+    """Check if a given list has duplicate elements"""
     return len(lst) == len(set(lst))
 
 
-def chunk(lst, chunk_size):
-    """
-    Split a list into a list of smaller lists defined by chunk_size
-    >>> chunk([1,2,4,5,6,6],5)
-    >>> chunk([1,2,4,5,6,6],2)
-    """
+def chunk(lst: list, chunk_size: int) -> list:
+    """Split a list into a list of smaller lists defined by chunk_size"""
     return list(
         map(lambda x: lst[x * chunk_size: x * chunk_size + chunk_size],
             list(range(0, ceil(len(lst) / chunk_size))))
     )
 
 
-def count_occurences(lst, value):
-    """
-    Function to count occurrences of value in a list
-    >>> count_occurences(lst=[1,2,3,4,4,4,4], value=4) # 4
-    """
+def count_occurrences(lst: list, value: Union[bool, str, int, float]) -> int:
+    """Function to count occurrences of value in a list"""
     return len([x for x in lst if x == value and type(x) == type(value)])
 
 
-def flatten(lst):
-    """
-    Flatten a list using recursion
-    >>> flatten(lst=[1,2,[3,4,5,[6,7]]]) # [1, 2, 3, 4, 5, 6, 7]
-    """
+def flatten(lst: list) -> list:
+    """Flatten a list using recursion"""
     res = []
     res.extend(flatten_list(list(map(lambda x: flatten(x) if type(x) == list else x, lst))))
     return res
 
 
-def flatten_list(arg):
-    """Flatten lists
-    Function used for flattening lists
-    >>> flatten_list([2,3,5,[7,8]])
-    """
+def flatten_list(arg) -> list:
+    """Function used for flattening lists recursively"""
     ls = []
     for i in arg:
         if isinstance(i, list):
@@ -69,6 +50,7 @@ def flatten_list(arg):
         else:
             ls.append(i)
     return ls
+
 
 if __name__ == '__main__':
     pass
