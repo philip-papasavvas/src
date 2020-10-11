cf27a26507bb7fa6961dbf09bc909d7868a54300
philip-papasavvas <39246705+philip-papasavvas@users.noreply.github.com>
2020-10-11 14:09:39 +0100

    refactored securityAnalysis\stationarity.py to make docstrings clearer, method names more meaningful, added static Typing, and added working examples at the bottom of the script

--- src/securityAnalysis/stationarity.py
+++ src/securityAnalysis/stationarity.py
@@ -1,12 +1,11 @@
 """
-Created on 30 June 2019
+Created on: 30 June 2019
 
-Look at the stationarity of stock time series, carry out analysis on the log-returns of a stock.
+Investigate stationarity of time series (example of security data), analytics on log-returns
 Provide summary statistics on the data
 Introduce tests (such as Augmented Dickey Fuller) to check stationarity of time series
+Inspiration from: https://www.analyticsvidhya.com/blog/2018/09/non-stationary-time-series-python/
 """
-import os
-
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -14,33 +13,31 @@ import scipy.stats as stats
 from scipy.stats import kurtosis, skew
 from statsmodels.tsa.stattools import adfuller
 
-import utils_date
-from securityAnalysis.utils_finance import calculate_log_return_df
+from securityAnalysis.utils_finance import calculate_return_df
 
 pd.set_option('display.max_columns', 10)
 pd.set_option('display.width', 500)
 plt.style.use('seaborn')
 
 
-def drop_null_columns(data: pd.DataFrame):
-    """Drop columns from the dataframe with null values"""
-    original_columns = list(data.columns)
-    cleaned_data = data.dropna(axis=1)
-    new_columns = list(cleaned_data.columns)
-    cut_columns = [x for x in original_columns if x not in new_columns]
-
-    print(f"The following columns have been dropped from the dataframe as they contain NaNs: "
-          f"\n {cut_columns}")
-    return cleaned_data
-
-
-def dickey_fuller_test(time_series):
+def test_stationarity_adf(time_series: np.array) -> None:
     """
-    Perform Dickey-Fuller test:
-    If the test statistic is less than the critical value then reject H_0, and the times series
-    is stationary.
-    If test statistic greater than critcal value, fail to reject H_0, and time series is
-    non-stationary/it has time dependence
+    Wrapper on adfuller method from statsmodels package, to perform Dickey-Fuller test for
+    Stationarity
+
+    Parameter:
+        time_series: time series containing non-null values which to perform stationarity test on
+
+    Returns
+        None: Print statement of ['Test Statistic', 'p-value', '# lags', '# observations', and
+        critical values for alpha 1, 5 and 10%
+
+    NOTE:
+        Test statistic: t
+        Critical value, c
+        Null hypothesis, H_0
+        If t < c, reject H_0 --> time series is stationary
+        If t > c, fail to reject H_0 --> time series is non-stationary (has some drift with time)
     """
     print('Results of Dickey-Fuller Test:')
     df_test = adfuller(time_series, autolag='AIC')
@@ -52,36 +49,54 @@ def dickey_fuller_test(time_series):
     print(df_output)
 
 
-def adf_test_result(timeseries, sig=5):
-    # Perform Dickey-Fuller test and give True/False for stationarity of time series.
-    # If True (test statistic less than critical value at 5%), reject H_0, and time series
-    # stationarity
-    # Offered at 1, 5 or 10% significance level
-    dftest = adfuller(timeseries, autolag='AIC')
-    assert (sig == 1) or (sig == 5) or (sig == 10), "Choose a significance of 1, 5 or 10 pc"
-    key = str(sig) + "%"
-    is_stationary = dftest[0] < dftest[4][key]
+def get_aug_dickey_fuller_result(time_series: np.array, alpha: int = 5) -> bool:
+    """
+    Method to perform Augmented Dickey Fuller Test for stationarity on time_series, at a
+    given level of significance alpha
+
+    Parameters:
+        time_series: 1-D array of time series data to be tested for stationarity
+        alpha: chosen level of significance, must be one of 1,5 or 10%
+
+    Returns:
+        bool: True if stationary data (t-statistic less than critical value at significance level
+        alpha, reject H_0), False for non-stationary data
+    """
+    assert alpha in [1, 5, 10], "Choose appropriate alpha significance: [1, 5 or 10%]"
+    print(f"Performing augmented Dickey Fuller test at significance level alpha: {alpha}")
+
+    df_test = adfuller(time_series, autolag='AIC')
+    test_stats = {
+        'test_statistic': df_test[0],
+        'p-values': df_test[4]
+    }
+    is_stationary = test_stats['test_statistic'] < test_stats['p-values'][f"{str(alpha)}%"]
+
     return is_stationary
 
 
-# create high level summary stats for the log returnss
-def get_descriptive_stats(data: pd.DataFrame, alpha: float = 0.05) -> pd.DataFrame:
-    """Compute descriptive stats (p-values given for two tailed tests)
+def get_descriptive_stats(data: pd.DataFrame, alpha: float = 0.05) -> dict:
+    """Compute descriptive, high level stats (p-values given for two tailed tests),
+    incuding skewness and kurtosis, specifying alpha (for tests of skewness and kurtosis)
 
     Args:
         data: Clean dataframe with no NaNs
         alpha: level of significance for the two-tailed test. must lie between 0 and 1
 
     Returns
-        pd.DataFrame
+        dict of results for descriptive level statistics
     """
+    assert 0 < alpha < 1, f"Alpha level of {alpha} is not valid, must lie between 0 and 1"
+    print("Getting descriptive level stats for dataframe...")
+
     result_df = pd.DataFrame(columns=['Size', 'Mean', 'Std Dev', 'Skewness', 'Excess Kurtosis'])
-    # , 'K Min', 'K Max'])
+
     result_df['Size'] = data.count()
     result_df['Mean'] = data.mean()
     result_df['Std Dev'] = np.std(data)
     result_df['Min'] = np.min(data)
     result_df['Max'] = np.max(data)
+
     result_df['Skewness'] = skew(data)
     result_df['Skewness t-statistic'] = \
         result_df['Skewness'].values / np.sqrt(6 / result_df['Size'].values)
@@ -92,70 +107,62 @@ def get_descriptive_stats(data: pd.DataFrame, alpha: float = 0.05) -> pd.DataFra
     result_df['Skewness accept H_0'] = skew_h0_values
     result_df.rename(columns={'Skewness accept H_0': skew_h0_title}, inplace=True)
 
-    result_df['Excess Kurtosis'] = kurtosis(data)  # if high excess kurtosis it means heavy tails
+    result_df['Excess Kurtosis'] = kurtosis(data)  # if high excess kurtosis --> thick tails
     result_df['Excess Kurtosis t-statistic'] = \
         result_df['Excess Kurtosis'].values / np.sqrt(24 / result_df['Size'].values)
     result_df['Excess Kurtosis p-value'] = \
         2 * (1 - stats.t.cdf(result_df['Excess Kurtosis t-statistic'], df=1))
-    kurt_h0_title = "Kurtosis reject H_0 at " + str(100 * alpha) + "% sig level"
+    kurt_h0_title = f"Kurtosis reject H_0 at {str(100 * alpha)}% sig level"
     kurt_h0_values = result_df['Excess Kurtosis p-value'].values < alpha
     result_df['Excess Kurtosis accept H_0'] = kurt_h0_values
     result_df.rename(columns={'Excess Kurtosis accept H_0': kurt_h0_title}, inplace=True)
 
-    res = []
+    adf_results = []
     for i in data.columns:
-        res.append(adf_test_result(data.loc[:, i]))
+        adf_results.append(get_aug_dickey_fuller_result(data.loc[:, i]))
 
-    result_df['Aug Dickey-Fuller Test'] = res
+    result_df['Aug Dickey-Fuller Test'] = adf_results
+    result_dict = result_df.T.to_dict()
 
-    return result_df
+    return result_dict
 
 
 if __name__ == '__main__':
 
-    # parameters
-    wk_dir = "C://Users//Philip//Documents//python//"
-    input_folder, output_folder = os.path.join(wk_dir, "input"), os.path.join(wk_dir, "output")
-    # "example_data.csv", "example_data_na.csv" has NA rows
-    # df = pd.read_csv(input_folder + 'example_data.csv') #, parse_dates=True)
+    # examples of running the above methods
 
-    # data read & clean
-    df = pd.read_csv(input_folder + "funds_stocks_2019.csv")
-    df = utils_date.char_to_date(df)
-    df.set_index('Date', inplace=True)
-    df_returns = calculate_log_return_df(data=df)
-    clean_df_returns = drop_null_columns(df_returns)
+    # ----------------
+    # real market data
+    # ----------------
+    import yfinance
+    price_series = yfinance.download(tickers='GOOGL', start="2010-01-01")['Adj Close'] # google data
+    price_df = pd.DataFrame(price_series)
 
-    # adf_test_result(clean_df_returns.iloc[:,0])
+    # -------------------
+    # random data example
+    # -------------------
+    import datetime
+    date_rng = pd.date_range(datetime.datetime.now().strftime("%Y-%m-%d"), periods=500).to_list()
+    random_returns = pd.Series(np.random.randn(500), index=date_rng)
+    price_series = random_returns.cumsum()
+    price_df = pd.DataFrame(price_series)
 
-    # ADF pulled from https://www.analyticsvidhya.com/blog/2018/09/non-stationary-time-series-python/
+    # run analysis
+    returns_df = calculate_return_df(data=price_df,
+                                     is_relative_return=True)
 
-    # -----------------
-    # TEST STATIONARITY
-    # -----------------
+    # # could also look at log returns of the data and see if the time series is stationary
+    # log_returns_df = calculate_return_df(data=price_df,
+    #                                      is_log_return=True)
 
-    # Test stationarity of a time series: use Augmented Dickey Fuller test
-    get_descriptive_stats(data=clean_df_returns, alpha=0.05)
 
-    # Look at the price time series first:
-    df = pd.read_csv(input_folder + "funds_stocks_2019.csv")
-    df = utils_date.char_to_date(df)  # convert all dates to np datetime64
-    df.set_index('Date', inplace=True)
+    # test for stationarity (using Augmented Dickey Fuller test) for one timeseries
+    test_stationarity_adf(time_series=price_series)
 
-    # Drop the null columns - but do not take log returns
-    clean_df = drop_null_columns(df)
+    # augmented dickey fuller result
+    get_aug_dickey_fuller_result(time_series=price_series)
 
-    result_prices = get_descriptive_stats(data=clean_df, alpha=0.05)
-    # are any of the time series stationary?
-    any(result_prices['Aug Dickey-Fuller Test'])  # False
-    # no, they are all time dependent
-
-    # Example: adf_test(clean_df_returns.iloc[:,0])
-
-    # Now look at log returns
-    clean_df_returns = calculate_log_return_df(df)
-    clean_df_log_rets = drop_null_columns(clean_df_returns)
-    result_returns = get_descriptive_stats(data=clean_df_log_rets, alpha=0.05)
-
-    # are any of the time series stationary?
-    all(result_returns['Aug Dickey-Fuller Test'])  # True, so they are stationary returns
+    # more descriptive statistics on skewness, kurtosis, as well as # observations, max, min, mean,
+    # standard deviation etc
+    get_descriptive_stats(data=returns_df,
+                          alpha=0.05)
