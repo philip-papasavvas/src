37d6bde4baef14695cc4d000ea295eeb7c948e0f
philip-papasavvas <39246705+philip-papasavvas@users.noreply.github.com>
2024-09-02 12:29:02 +0100

    change to stationarity.py to improve docstrings and code

--- src/securityAnalysis/stationarity.py
+++ src/securityAnalysis/stationarity.py
@@ -1,9 +1,11 @@
 """
 Created on: 30 June 2019
 
-Investigate stationarity of time series (example of security data), analytics on log-returns
-Provide summary statistics on the data
-Introduce tests (such as Augmented Dickey Fuller) to check stationarity of time series
+Module for investigating the stationarity of time series data, specifically focusing
+on security data.
+Provides summary statistics, log-return analytics, and tests for stationarity using
+methods like the Augmented Dickey-Fuller test.
+
 Inspiration from: https://www.analyticsvidhya.com/blog/2018/09/non-stationary-time-series-python/
 """
 import matplotlib.pyplot as plt
@@ -17,62 +19,64 @@ from securityAnalysis.utils_finance import calculate_security_returns
 
 pd.set_option('display.max_columns', 10)
 pd.set_option('display.width', 500)
+
+# Set plot style
 plt.style.use('seaborn')
 
 
 def test_stationarity_adf(time_series: np.array) -> None:
     """
-    Wrapper on adfuller method from statsmodels package, to perform Dickey-Fuller test for
-    Stationarity
+    Perform the Augmented Dickey-Fuller test for stationarity on a given time series.
+    (Wrapper on adfuller method from statsmodels package)
 
-    Parameter:
-        time_series: time series containing non-null values which to perform stationarity test on
+    Parameters:
+        time_series (np.ndarray): Time series data to test for stationarity.
 
-    Returns
-        None: Print statement of ['Test Statistic', 'p-value', '# lags', '# observations', and
-        critical values for alpha 1, 5 and 10%
+    Returns:
+        None: Prints the test results, including the test statistic, p-value,
+        number of lags, number of observations, and critical values for alpha
+        1%, 5%, and 10%.
 
     NOTE:
         Test statistic: t
         Critical value, c
         Null hypothesis, H_0
         If t < c, reject H_0 --> time series is stationary
-        If t > c, fail to reject H_0 --> time series is non-stationary (has some drift with time)
+        If t > c, fail to reject H_0 --> time series is non-stationary
+            (has some drift with time)
     """
     print('Results of Dickey-Fuller Test:')
-    df_test = adfuller(time_series, autolag='AIC')
-    df_output = pd.Series(df_test[0:4],
-                          index=['Test Statistic', 'p-value', '#Lags Used',
-                                 'Number of Observations Used'])
-    for key, value in df_test[4].items():
-        df_output['Critical Value (%s)' % key] = value
+    adf_result = adfuller(time_series, autolag='AIC')
+    df_output = pd.Series(
+        adf_result[0:4],
+        index=['Test Statistic', 'p-value', '#Lags Used', 'Number of Observations Used'])
+    for key, value in adf_result[4].items():
+        df_output[f'Critical Value ({key})'] = value
     print(df_output)
 
 
 def get_aug_dickey_fuller_result(time_series: np.array, alpha: int = 5) -> bool:
     """
-    Method to perform Augmented Dickey Fuller Test for stationarity on time_series, at a
-    given level of significance alpha
+    Perform the Augmented Dickey-Fuller Test for stationarity on a time series.
 
     Parameters:
-        time_series: 1-D array of time series data to be tested for stationarity
-        alpha: chosen level of significance, must be one of 1,5 or 10%
+        time_series (np.ndarray): 1-D array of time series data.
+        alpha (int): Significance level (1, 5, or 10%).
 
     Returns:
-        bool: True if stationary data (t-statistic less than critical value at significance level
-        alpha, reject H_0), False for non-stationary data
+        bool: True if the series is stationary (rejects the null hypothesis),
+        False otherwise.
     """
     assert alpha in [1, 5, 10], "Choose appropriate alpha significance: [1, 5 or 10%]"
-    print(f"Performing augmented Dickey Fuller test at significance level alpha: {alpha}")
+    print(f"Performing augmented Dickey Fuller test at significance level alpha: "
+          f"{alpha}")
 
-    df_test = adfuller(time_series, autolag='AIC')
-    test_stats = {
-        'test_statistic': df_test[0],
-        'p-values': df_test[4]
-    }
-    is_stationary = test_stats['test_statistic'] < test_stats['p-values'][f"{str(alpha)}%"]
+    adf_result = adfuller(time_series, autolag='AIC')
 
-    return is_stationary
+    test_statistic = adf_result[0],
+    critical_value = adf_result[4][f'{alpha}%']
+
+    return test_statistic < critical_value
 
 
 def get_descriptive_stats(data: pd.DataFrame, alpha: float = 0.05) -> dict:
@@ -80,16 +84,20 @@ def get_descriptive_stats(data: pd.DataFrame, alpha: float = 0.05) -> dict:
     incuding skewness and kurtosis, specifying alpha (for tests of skewness and kurtosis)
 
     Args:
-        data: Clean dataframe with no NaNs
-        alpha: level of significance for the two-tailed test. must lie between 0 and 1
+        data (pd.DataFrame): DataFrame with no NaNs containing the data to analyze.
+        alpha (float): Significance level for the two-tailed test.
+            Must be between 0 and 1.
 
     Returns
         dict of results for descriptive level statistics
     """
-    assert 0 < alpha < 1, f"Alpha level of {alpha} is not valid, must lie between 0 and 1"
+    assert 0 < alpha < 1, f"Alpha level of {alpha} is not valid, must lie between 0 " \
+                          f"and 1"
     print("Getting descriptive level stats for dataframe...")
 
-    result_df = pd.DataFrame(columns=['Size', 'Mean', 'Std Dev', 'Skewness', 'Excess Kurtosis'])
+    result_df = pd.DataFrame(
+        columns=['Size', 'Mean', 'Std Dev', 'Skewness', 'Excess Kurtosis']
+    )
 
     result_df['Size'] = data.count()
     result_df['Mean'] = data.mean()
@@ -100,8 +108,10 @@ def get_descriptive_stats(data: pd.DataFrame, alpha: float = 0.05) -> dict:
     result_df['Skewness'] = skew(data)
     result_df['Skewness t-statistic'] = \
         result_df['Skewness'].values / np.sqrt(6 / result_df['Size'].values)
-    result_df['Skewness p-value'] = 2 * (1 - stats.t.cdf(result_df['Skewness t-statistic'], df=1))
-    # so, one can reject h_0 (skewness of log returns = 0) for a p-value of less than alpha
+    result_df['Skewness p-value'] = \
+        2 * (1 - stats.t.cdf(result_df['Skewness t-statistic'], df=1))
+    # so, one can reject h_0 (skewness of log returns = 0) for a
+    # p-value of less than alpha
     skew_h0_title = "Skewness reject H_0 at " + str(100 * alpha) + "% sig level"
     skew_h0_values = result_df['Skewness p-value'].values < alpha
     result_df['Skewness accept H_0'] = skew_h0_values
@@ -130,32 +140,41 @@ def get_descriptive_stats(data: pd.DataFrame, alpha: float = 0.05) -> dict:
 if __name__ == '__main__':
     # real market data
     import yfinance
-    price_series = yfinance.download(tickers='GOOGL', start="2010-01-01")['Adj Close'] # google data
-    price_df = pd.DataFrame(price_series)
+    import datetime
+
+    price_series_stock = yfinance.download(tickers='GOOGL', start="2010-01-01")['Adj Close'] # google data
+    price_df = pd.DataFrame(price_series_stock)
 
     # random data example
-    import datetime
-    date_rng = pd.date_range(datetime.datetime.now().strftime("%Y-%m-%d"), periods=500).to_list()
+    date_rng = pd.date_range(
+        datetime.datetime.now().strftime("%Y-%m-%d"), periods=500
+    ).to_list()
     random_returns = pd.Series(np.random.randn(500), index=date_rng)
-    price_series = random_returns.cumsum()
-    price_df = pd.DataFrame(price_series)
+    price_series_random = random_returns.cumsum()
 
     # run analysis
-    returns_df = calculate_security_returns(data=price_df,
-                                            is_relative_return=True)
+    returns_df = calculate_security_returns(
+        data=pd.DataFrame(price_series_random),
+        is_relative_return=True
+    )
 
     # # could also look at log returns of the data and see if the time series is stationary
     # log_returns_df = calculate_security_returns(data=price_df,
     #                                      is_log_return=True)
 
 
-    # test for stationarity (using Augmented Dickey Fuller test) for one timeseries
-    test_stationarity_adf(time_series=price_series)
+    # Test for stationarity
+    test_stationarity_adf(time_series=price_series_stockrandom)
 
     # augmented dickey fuller result
-    get_aug_dickey_fuller_result(time_series=price_series)
+    is_stationary = get_aug_dickey_fuller_result(time_series=price_series_stockrandom)
+    print(f"Is the time series stationary?: {is_stationary}")
 
-    # more descriptive statistics on skewness, kurtosis, as well as # observations, max, min, mean,
+    # more descriptive statistics on skewness, kurtosis,
+    # as well as # observations, max, min, mean,
     # standard deviation etc
-    get_descriptive_stats(data=returns_df,
-                          alpha=0.05)
+    descriptive_stats = get_descriptive_stats(
+        data=returns_df,
+        alpha=0.05
+    )
+    print(descriptive_stats)
